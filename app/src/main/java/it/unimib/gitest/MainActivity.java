9f446e0c514ccef63ae379fce3d8f0421a84d1b9
Giacomo Mele <g.mele9@campus.unimib.it>
2020-04-15 17:49:26 +0200

    Comment from master branch

--- app/src/main/java/it/unimib/gitest/MainActivity.java
+++ app/src/main/java/it/unimib/gitest/MainActivity.java
@@ -3,7 +3,9 @@ package it.unimib.gitest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+/*
+    MainActivity
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
