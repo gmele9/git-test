c848294453d152b9141f9994df7923efae3383f0
Giacomo Mele <g.mele9@campus.unimib.it>
2020-04-15 17:36:50 +0200

    Commit from test branch

--- app/src/main/java/it/unimib/gitest/MainActivity.java
+++ app/src/main/java/it/unimib/gitest/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // a commento from test branch
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
